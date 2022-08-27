051463d67e09c61701b1dde5ee3a17d9d2232617
QijingZheng <zqj.kaka@gmail.com>
2022-08-27 20:52:57 +0800

    move metadata to setup.cfg

--- setup.py
+++ setup.py
@@ -1,15 +1,6 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
-setup(
-    name         = 'pySBT',
-    version      = '1.0',
-    description  = 'A python implementation of spherical Bessel transform (SBT) based on algorithm proposed by J. Talman.'
-    author       = 'Qijing Zheng',
-    author_email = 'zqj@ustc.edu.cn',
-    url          = 'https://github.com/QijingZheng/pySBT',
-    py_modules   = [
-        'pysbt',
-    ],
-)
+if __name__ == "__main__":
+    setup()
